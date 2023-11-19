07c6fda9e620d64e4fc4827d661fc7be37cbdf14
henrikoldcorn <holdcorn@gmail.com>
2023-11-19 17:01:15 +0000

    Add files via upload

---
+++ phoDeDupGUI.py
@@ -0,0 +1,127 @@
+import PySimpleGUI as sg
+import phoDeDupLib as pddl
+import os
+import logging
+
+#setup logfile
+logging.basicConfig(filename="phoDeDup.log", format='%(asctime)s %(levelname)-8s %(message)s', 
+	encoding="utf-8", level=logging.INFO)
+logging.info("started phoDeDup")
+
+
+sg.theme("DarkAmber")
+
+#define the main window
+selected_folders = []
+lst = sg.Listbox(values=selected_folders, size=(60, 6), key="-LISTBOX-")
+layout = [  [sg.Checkbox("debug", default=False, key="-debug-", enable_events=True, change_submits=True)],
+			[sg.Text("Select the folder(s) to remove duplicates from:")], 
+			[sg.Text("Folder:"), sg.In(size=(60,1), enable_events=True, key="-FOLDER-"), sg.FolderBrowse()],
+			[sg.Button("Add folder to selection", key="Add"), sg.Button("Remove")],
+			[lst], 
+			[sg.Button("Find duplicate images", key="Run"), sg.Button("Exit")]
+		]
+
+window = sg.Window("Photo De-Duplicator", layout, resizable=True)
+
+
+#define secondary file selection window
+def popup():
+	#size of thumbnails to display
+	thumbsize = (100, 100)
+	
+	item_list = []
+	checkbox_keys = []
+	for key in dupes.keys():
+		
+		#generate checkbox keys for later use
+		for f in dupes[key]:
+			checkbox_keys.append(f)
+			
+		#for each hash only read the image once (because identical),
+		#so read the first one, dupes[key][0]
+		
+		#nasty way of getting nested items: left-aligned photo, 
+		#with filenames and checkboxes vertically to the right
+		checkboxes = [ [sg.Checkbox(x, key=x)] for x in dupes[key] ]
+		#print(checkboxes[0][-1:])
+		item_list.append([sg.Image(data=pddl.convert_to_bytes(dupes[key][0], thumbsize)), 
+							sg.Button("Open\nimage", size=(5, 2), key="imagebutton"+dupes[key][0]),
+							sg.Column(checkboxes, pad=20)
+						])
+	#item_list = item_list[-100:]
+	#item_list.append([sg.Text("No more items")])
+	#print("item_list {}".format(item_list[-2:]))
+	#column = 
+	layout_sec = [	[sg.Text("{} duplicated photos found.".format(len(dupes)))],
+					[sg.Text("Select files to delete. Selecting all items from a photo will delete all copies of that photo!")], 
+					[sg.Column(item_list, size=(1000, 500), scrollable=True, vertical_scroll_only=False)],
+					[sg.Button("Close"), sg.Button("Delete selected files", key="Delete")]
+				]
+	
+	window_sec = sg.Window("PDD", layout_sec, modal=True, resizable=True)
+	
+	while True:
+		event_sec, values_sec = window_sec.read()
+		if event_sec == sg.WIN_CLOSED or event_sec == "Close":
+			break
+		if event_sec.startswith("imagebutton"):
+			os.startfile(event_sec[11:])  #strip out the "imagebuttton" from button/filename
+		if event_sec == "Delete":
+			files_to_delete = []
+			ftd_counter = 0
+			for f in checkbox_keys:
+				checkbox = window_sec.Element(f)
+				if checkbox.get():
+					files_to_delete.append(f)
+					ftd_counter += 1
+			
+			if not ftd_counter:
+				sg.popup_ok("No files selected")
+			elif sg.popup_ok_cancel("Are you sure you want to delete {} files?".format(ftd_counter)) == "OK":
+				for f in files_to_delete:
+					try:
+						os.remove(f)
+						logging.info("deleted {}".format(f))
+					except:
+						logging.warning("failed to delete {}".format(f))
+				sg.popup_ok("Files deleted")
+				break
+	window_sec.close()
+
+dir = False
+dupes = []
+debug_old = False
+
+while True:
+	event, values = window.read()
+	if event == sg.WIN_CLOSED or event == "Exit":
+		break
+	debug_checkbox = values["-debug-"]
+	if debug_checkbox != debug_old:
+		debug_old = debug_checkbox
+		#has debug just been set to True?
+		if debug_old:
+			logging.getLogger().setLevel(logging.DEBUG)
+			logging.info("level set to DEBUG")
+		else:
+			logging.getLogger().setLevel(logging.INFO)
+			logging.info("level set to INFO")
+	if event == "-FOLDER-":
+		dir = values["-FOLDER-"].replace("/", os.sep)
+		selected_folders.append(dir)
+	if event == "Add" and dir:
+		window.Element("-LISTBOX-").Update(values=selected_folders)
+	if event == "Remove":
+		val = lst.get()[0]
+		selected_folders.remove(val)
+		window.Element("-LISTBOX-").Update(values=selected_folders)
+	if event == "Run" and selected_folders:
+		logging.info("analysing {}".format(str(selected_folders)))
+		dupes = pddl.getDupes(selected_folders)
+		select_all = False
+		popup()
+		
+			
+window.close()
+logging.info("closed phoDeDup")
---
+++ phoDeDupLib.py
@@ -0,0 +1,85 @@
+import os, hashlib
+import io
+import PIL
+from PIL import Image
+import base64
+
+def getAllFiles(path):
+	files = []
+	thisDir = os.scandir(path)
+	for i in thisDir:
+		if i.is_file():
+			files.append(i.path)
+		elif i.is_dir():
+			for x in getAllFiles(i.path):
+				files.append(x)
+	return files
+	
+def sha256sum(filename):
+    with open(filename, 'rb', buffering=0) as f:
+        return hashlib.file_digest(f, 'sha256').hexdigest()
+		
+def getDupes(directories, verbose=True) -> dict:
+	#directories to dedupe, print text?
+	hashes = {}
+	for folder in directories:
+		allFiles = getAllFiles(folder)
+		#select all the .jpg files
+		jpgs = [x for x in allFiles if x.lower().endswith(".jpg")]
+		counter = 0
+		for x in jpgs:
+			h = sha256sum(x)
+			if h in hashes:
+				hashes[h].append(x)
+			else:
+				hashes[h] = [x]
+
+	hashes_copy = {}
+	#print("hashes length {}".format(len(hashes)))
+	for key in hashes.keys():
+		#logging.info(key)
+		#print("key: " + key)
+		value = hashes[key]	#is a list
+		if len(value) > 1: # is there more than one entry?
+			#if len(hashes_copy) > 500:
+			#	print(len(hashes_copy))
+			#print("added " + str(key))
+			hashes_copy[key] = value
+			#print(value)
+	#print(hashes_copy)
+	#print("hashes_copy length {}".format(len(hashes_copy)))
+	return hashes_copy
+	
+def convert_to_bytes(file_or_bytes, resize=None, fill=False):
+	'''
+	Will convert into bytes and optionally resize an image that is a file or a base64 bytes object.
+	Turns into  PNG format in the process so that can be displayed by tkinter
+	:param file_or_bytes: either a string filename or a bytes base64 image object
+	:type file_or_bytes:  (Union[str, bytes])
+	:param resize:  optional new size
+	:type resize: (Tuple[int, int] or None)
+	:return: (bytes) a byte-string object
+	:rtype: (bytes)
+	'''
+	#https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Image_Viewer_Thumbnails.py
+
+	if isinstance(file_or_bytes, str):
+			img = PIL.Image.open(file_or_bytes)
+	else:
+		try:
+			img = PIL.Image.open(io.BytesIO(base64.b64decode(file_or_bytes)))
+		except Exception as e:
+			dataBytesIO = io.BytesIO(file_or_bytes)
+			img = PIL.Image.open(dataBytesIO)
+
+	cur_width, cur_height = img.size
+	if resize:
+		new_width, new_height = resize
+		scale = min(new_height / cur_height, new_width / cur_width)
+		img = img.resize((int(cur_width * scale), int(cur_height * scale)), PIL.Image.LANCZOS)
+	if fill:
+		img = make_square(img, THUMBNAIL_SIZE[0])
+	with io.BytesIO() as bio:
+		img.save(bio, format="PNG")
+		del img
+		return bio.getvalue()
