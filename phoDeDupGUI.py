879100a18b08e73bbd5e76b49a532074ff59df8a
henrikoldcorn <holdcorn@gmail.com>
2023-11-26 13:23:07 +0000

    Update phoDeDupGUI.py
    Added multiple pages, to get past Tkinter's apparent 10k widget limit

--- phoDeDupGUI.py
+++ phoDeDupGUI.py
@@ -5,9 +5,15 @@ import logging
 
 
 #setup logfile
+logger = logging.getLogger('pdd')
 logging.basicConfig(filename="phoDeDup.log", format='%(asctime)s %(levelname)-8s %(message)s', 
 	encoding="utf-8", level=logging.INFO)
-logging.info("started phoDeDup")
+logger.info("started phoDeDup")
+
+#setup variables
+PAGESIZE = 200
+#used to abort multiple windows
+#ABORT = False
 
 
 sg.theme("DarkAmber")
@@ -28,48 +34,68 @@ window = sg.Window("Photo De-Duplicator", layout, resizable=True)
 
 
 #define secondary file selection window
-def popup():
+def popup(duplicates, totalDupes, thisPage, pageCount):
+
+	#********************************
+	#popup window contains: a Column, which contains in one list an Image, a Button, and a Column, again containing a list of checkboxes, labelled with a filename
+	#building this happens inside-out
+	#********************************
+	#dupes
+	
 	#size of thumbnails to display
 	thumbsize = (100, 100)
 	
 	item_list = []
 	checkbox_keys = []
-	for key in dupes.keys():
+	for key in duplicates.keys():
 		
 		#generate checkbox keys for later use
-		for f in dupes[key]:
+		for f in duplicates[key]:
 			checkbox_keys.append(f)
 			
-		#for each hash only read the image once (because identical),
+		#for each hash only load the image once (because identical),
 		#so read the first one, dupes[key][0]
 		
 		#nasty way of getting nested items: left-aligned photo, 
 		#with filenames and checkboxes vertically to the right
-		checkboxes = [ [sg.Checkbox(x, key=x)] for x in dupes[key] ]
+		checkboxes = [ [sg.Checkbox(x, key=x)] for x in duplicates[key] ]
 		#print(checkboxes[0][-1:])
 		try:
-			img = pddl.convert_to_bytes(dupes[key][0], thumbsize)
+			img = pddl.convert_to_bytes(duplicates[key][0], thumbsize)
 		except:
 			img = bytes(0) # empty image, handles failed file read
 		item_list.append([sg.Image(data=img), 
-							sg.Button("Open\nimage", size=(5, 2), key="imagebutton"+dupes[key][0]),
+							sg.Button("Open\nimage", size=(5, 2), key="imagebutton"+duplicates[key][0]),
 							sg.Column(checkboxes, pad=20)
 						])
-	#item_list = item_list[-100:]
-	#item_list.append([sg.Text("No more items")])
-	#print("item_list {}".format(item_list[-2:]))
-	#column = 
-	layout_sec = [	[sg.Text("{} duplicated photo(s) found.".format(len(dupes)))],
+						
+	#which photo range, from 0, dupesLen are we currently showing
+	start = ((thisPage-1)*PAGESIZE) + 1
+	stop = start + PAGESIZE - 1
+	#unless we are on the last page, which likely won't be the full size:
+	if thisPage == pageCount:
+		stop = totalDupes
+	
+	layout_sec = [	[sg.Text("{} duplicated photo(s) found, this is page {} of {}.".format(totalDupes, thisPage, pageCount))],
+					[sg.Text("Showing photos {}-{}.".format(start, stop))],
 					[sg.Text("Select files to delete. You can select betwen none and all files for each photo.\nSelecting all items from a photo will delete all copies of that photo!", size=(None, 2))], 
 					[sg.Column(item_list, size=(1000, 500), scrollable=True, vertical_scroll_only=False)],
-					[sg.Button("Close"), sg.Button("Delete selected files", key="Delete")]
+					[sg.Text("'Close' may take some time to open the next window, be patient. ")],
+					[sg.Button("Delete selected files and open the next page of photos", key="Delete"), sg.Button("Close this window and open the next page of photos", key="Close"), sg.Button("Exit and return to folder selection", key="Abort")]
 				]
-	
+	#remove "Close" button if we're on the last page
+	if thisPage == pageCount:
+		del layout_sec[-1][1]
+		
 	window_sec = sg.Window("PDD", layout_sec, modal=True, resizable=True)
 	
+	ABORT = False
 	while True:
 		event_sec, values_sec = window_sec.read()
 		if event_sec == sg.WIN_CLOSED or event_sec == "Close":
+			break
+		if event_sec == "Abort":
+			ABORT = True
 			break
 		if event_sec.startswith("imagebutton"):
 			os.startfile(event_sec[11:])  #strip out the "imagebuttton" from button/filename
@@ -89,13 +115,14 @@ def popup():
 				for f in files_to_delete:
 					try:
 						os.remove(f)
-						logging.info("deleted {}".format(f))
+						logger.info("deleted {}".format(f))
 					except:
-						logging.warning("failed to delete {}".format(f))
+						logger.warning("failed to delete {}".format(f))
 						failure_message = "\nSome files were skipped due to an error;\nSee phoDeDup.log for details"
 				sg.popup_ok("Files deleted"+failure_message)
 				break
 	window_sec.close()
+	return ABORT
 
 dir = False
 dupes = []
@@ -110,11 +137,11 @@ while True:
 		debug_old = debug_checkbox
 		#has debug just been set to True?
 		if debug_old:
-			logging.getLogger().setLevel(logging.DEBUG)
-			logging.info("level set to DEBUG")
+			logger.setLevel(logging.DEBUG)
+			logger.info("level set to DEBUG")
 		else:
-			logging.getLogger().setLevel(logging.INFO)
-			logging.info("level set to INFO")
+			logger.setLevel(logging.INFO)
+			logger.info("level set to INFO")
 	if event == "-FOLDER-":
 		dir = values["-FOLDER-"].replace("/", os.sep) 	#fixes mixed / and \
 		if os.path.isdir(dir) and dir not in selected_folders:
@@ -126,12 +153,31 @@ while True:
 		selected_folders.remove(val)
 		window.Element("-LISTBOX-").Update(values=selected_folders)
 	if event == "Run" and selected_folders:
-		logging.info("analysing folders: " + ", ".join(selected_folders))
+		logger.info("analysing folders: " + ", ".join(selected_folders))
 		dupes = pddl.getDupes(selected_folders)
-		logging.debug(str(len(dupes)) + "dupes found")
-		#select_all = False
-		popup()
 		
-			
+		dupesLen = len(dupes)
+		totalFiles = sum([len(list(dupes.values())[i]) for i in range(dupesLen)])
+		
+		logger.info(str(dupesLen) + " duplicated photos found")
+		logger.info(str(totalFiles) + " total files")
+		
+		
+		chunks = list(pddl.chunks(dupes, SIZE=PAGESIZE))
+		pages = len(chunks)
+		#ignore all input while popups are running
+		window.disable()
+		ABORT = False
+		for i in range(pages):
+			if not ABORT:
+				dupes_chunk = chunks[i]
+				#format: popup(duplicates, totalDupes, thisPage, pageCount)
+				ABORT = popup(dupes_chunk, dupesLen, i+1, pages)
+				
+		ABORT = False
+		window.enable()
+		
+#print(len(win
+
 window.close()
-logging.info("closed phoDeDup")
+logger.info("closed phoDeDup")
